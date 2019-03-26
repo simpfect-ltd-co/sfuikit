888d4679c54defb87adbe53fef213e04c8f49156
NguyenNT <nguyennt@simpfect.com>
2019-03-26 16:18:57 +0700

    input code

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,6 +6,15 @@ interface Props extends BaseProps {
   value?: string
   placeholder?: string
   type?: string
+  plain?: boolean
+  disabled?: boolean
+}
+interface InputProps {
+  onChange?: (value: string) => void
+  value?: string
+  placeholder?: string
+  type?: string
+  // 'font-size'?: string
 }
 
 export default class UIInput extends React.Component<Props> {
@@ -14,36 +23,123 @@ export default class UIInput extends React.Component<Props> {
   }
   render() {
     return (
-      <Wrapper>
-        <Label data-active={this.state.active || !!this.props.value}>
-          {this.props.placeholder}
-        </Label>
-        <Enhancer>
+      <Enhancer>
+        <Wrapper
+          {...this.props}
+          onChange={(e: any) => {
+            this.props.onChange && this.props.onChange(e.target.value)
+          }}
+          onClick={(e: any) => {
+            this.props.onClick && this.props.onClick(e)
+            this.setState({ active: true })
+            console.log('props', this.props)
+          }}
+          onBlur={() => {
+            this.setState({ active: false })
+          }}
+        >
+          <Label data-active={this.state.active || !!this.props.value}>
+            {this.props.placeholder}
+          </Label>
           <Input
             {...this.props}
-            default-style={(theme: any) => `             
-              width: 100%;             
-              height:32; 
-              font-size:16;                      
-              box-sizing: border-box;             
-              border: none;
-              background:none;
-              border-bottom: 1px solid ${theme.divider};             
-              padding:6 0 7 ;                                              
-              :hover {
-                border-bottom: 2px solid;
-              }
-              :focus {
-                outline: none; 
-                border-bottom: 2px solid ${
-                  theme.accent
-                };                                   
-              }             
-              ::placeholder{
-                opacity:0;                           
-              }
-              
-            `}
+            style={{ ...this.props }}
+            data-plain={this.props.plain}
+            data-disabled={this.props.disabled}
+          >
+            {this.props.children}
+          </Input>
+        </Wrapper>
+      </Enhancer>
+    )
+  }
+}
+
+const BaseInput = styled.input`
+  width: 100%;
+  margin-top: 16px;
+  border: none;
+  background: none;
+  border-bottom: 1px solid ${(p: any) => p.theme.divider};
+  padding: 6 0 7;
+  :hover {
+    border-bottom: 2px solid ${(p: any) => p.theme.primary_text};
+  }
+  :focus {
+    outline: none;
+    border-bottom: 2px solid ${(p: any) => p.theme.primary};
+  }
+  ::placeholder {
+    opacity: 0;
+  }
+  &[data-plain='true'] {
+    border: none;
+  }
+  &[data-disabled]='true'] {
+    border: red !important;
+  }
+`
+
+const Input = styled(BaseInput)`
+${(p: any) => p['sf-default-style'] && p['sf-default-style'](p.theme)}
+  ${(p: any) => p['sf-css']}
+  ${(p: any) => {
+    switch (p.variant) {
+      case 'plain':
+        return 'border: none !important;'
+      case 'disabled':
+        return (
+          'color:' +
+          p.theme.secondary_text +
+          '; border-bottom: 1px dotted' +
+          p.theme.secondary_text +
+          ';'
+        )
+      default:
+        return ''
+    }
+  }}
+`
+
+const Wrapper = styled(BaseView)`
+  position: relative;
+  // height: 48px;
+  margin: 5;
+`
+const Label = styled.div`
+  color: ${(p: any) => p.theme.secondary_text};
+  pointer-events: none;
+  height: 0;
+  // position: absolute;
+  // top: 0;
+  transform: translateY(19px);
+  transform-origin: top left;
+  transition: 0.2s;
+  &[data-active='true'] {
+    font-size: 12px;
+    transform: translate(0);
+    color: ${(p: any) => p.theme.primary};
+  }
+`
+
+export class MultilineInput extends React.Component<Props> {
+  state = {
+    active: false
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Enhancer>
+          <Label
+            {...this.props}
+            data-active={this.state.active || !!this.props.value}
+          >
+            {this.props.placeholder}
+          </Label>
+        </Enhancer>
+        <Enhancer>
+          <MultilineWrapper
+            {...this.props}
             onChange={(e: any) => {
               this.props.onChange && this.props.onChange(e.target.value)
             }}
@@ -54,35 +150,36 @@ export default class UIInput extends React.Component<Props> {
             onBlur={() => {
               this.setState({ active: false })
             }}
-          >
-            {this.props.children}
-          </Input>
+          />
         </Enhancer>
       </Wrapper>
     )
   }
 }
-const BaseInput = styled.input`
+
+const BaseMultiline = styled.textarea`
   ${(p: any) => p['sf-default-style'] && p['sf-default-style'](p.theme)}
   ${(p: any) => p['sf-css']}
 `
 
-const Input = styled(BaseInput)``
-
-const Wrapper = styled.div`
-  position: relative;
-  height: 48px;
-  margin: 5;
-`
-const Label = styled.div`
-  color: #757575;
-  font-size: 16;
-  pointer-events: none;
-  transform: translateY(24px);
-  transition: 0.2s;
-  transform-origin: top left;
-  &[data-active='true'] {
-    transform: scale(0.75);
-    color: #448aff;
-  }
+const MultilineWrapper = styled(BaseMultiline)`
+    margin-top:16px;
+    width:100%;
+    height:4rem;
+    border: none;
+    // background: transparent;
+    border-bottom: 1px solid ${(p: any) => p.theme.divider};
+    padding: 6px 0 7px;
+    resize: none;
+    }
+    :hover {
+    border-bottom: 2px solid ${(p: any) => p.theme.primary_text};
+    }
+    :focus {
+    outline: none;
+    border-bottom: 2px solid ${(p: any) => p.theme.primary}
+    }
+    ::placeholder{
+      opacity:0;
+    }
 `
--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import * as UI from 'components'
+import { MultilineInput } from 'components/Input'
 export default class Main extends React.Component {
   state = {
     value: false,
@@ -27,7 +28,7 @@ export default class Main extends React.Component {
 
                 <UI.Switch
                   value={this.state.value}
-                  padding="15"
+                  padding="30"
                   primaryColor={colors.primary}
                   secondaryColor={colors.light_primary}
                   onChange={(value: boolean) => {
@@ -36,11 +37,48 @@ export default class Main extends React.Component {
                 />
               </UI.View>
               <UI.Text>MATERIAL-UI</UI.Text>
+              <UI.Text>
+                {this.state.inputText ? this.state.inputText : 'Not value'}
+              </UI.Text>
               <UI.Input
+                // disabled
+                // plain
+                width="300"
+                // background="red"
+                font-size="40"
+                color-mobile="red"
+                variant="disabled"
+                // padding="40"
+                // type="number"
                 placeholder="Placeholder"
                 value={this.state.inputText}
                 onChange={val => this.setState({ inputText: val })}
               />
+              {/* <UI.Input
+                variant="plain"
+                font-size="32"
+                placeholder="Placeholder"
+                value={this.state.inputText}
+                onChange={val => this.setState({ inputText: val })}
+              />
+              <UI.Input
+                font-size="16"
+                placeholder="Placeholder"
+                value={this.state.inputText}
+                onChange={val => this.setState({ inputText: val })}
+              />
+              <MultilineInput
+                // padding="20"
+                // background="blue"
+                placeholder="Placeholder"
+                value={this.state.inputText}
+                onChange={val => this.setState({ inputText: val })}
+              />
+              <textarea
+                placeholder="Placeholder"
+                rows={5}
+                style={{ background: 'red' }}
+              /> */}
             </UI.View>
           )}
         </UI.ThemeValueProvider>
